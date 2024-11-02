70ff2990a7161d5d81aa83779fafd1fd09805d66
Lpedz <pkd22it014@gecskp.ac.in>
2024-11-02 15:12:26 +0530

    Initial Commit

---
+++ Apps/FlashLight/lib/main.dart
@@ -0,0 +1,63 @@
+import 'package:flutter/material.dart';
+import 'package:torch_light/torch_light.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flashlight App',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: FlashlightHomePage(),
+    );
+  }
+}
+
+class FlashlightHomePage extends StatefulWidget {
+  const FlashlightHomePage({super.key});
+
+  @override
+  _FlashlightHomePageState createState() => _FlashlightHomePageState();
+}
+
+class _FlashlightHomePageState extends State<FlashlightHomePage> {
+  bool _isFlashlightOn = false;
+
+  void _toggleFlashlight() async {
+    try {
+      if (_isFlashlightOn) {
+        await TorchLight.disableTorch(); // Method to turn off the flashlight
+      } else {
+        await TorchLight.enableTorch(); // Method to turn on the flashlight
+      }
+      setState(() {
+        _isFlashlightOn = !_isFlashlightOn;
+      });
+    } catch (e) {
+      // Handle any errors that may occur
+      print("Error toggling flashlight: $e");
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Flashlight App'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: _toggleFlashlight,
+          child: Text(_isFlashlightOn ? 'Turn Off' : 'Turn On'),
+        ),
+      ),
+    );
+  }
+}
